ec36ae33bd4ca87666dae683c421e9ea5dc32a69
mdkawsar <info.mdkawsardev@gmail.com>
2026-02-19 16:40:25 +0600

    Site has been ready with settings.py, urls.py, urls.py, views.py, templates

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index, name='home')
+    # path('', views.index, name='home'),
+    path('', views.your_view_name, name='your_view_name'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from .forms import FeedbackForm
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def your_view_name(request):
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            form.save()  # ডেটা ডাটাবেসে সেভ
+            return JsonResponse({'message': 'success'})
+        else:
+            return JsonResponse({'message': 'fail', 'errors': form.errors}, status=400)
+    else:
+        form = FeedbackForm()
+    return render(request, 'index.html', {'form': form})
